d978725ee5de45d4ae4e36482d61f74d33d52d24
Quantyc <quantyc61@gmail.com>
2025-05-12 16:15:46 +0700

    Update deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,1 +1,13 @@
+async function main() {
+  const [deployer] = await ethers.getSigners();
+  console.log("Deploying with:", deployer.address);
+
+  const Token = await ethers.getContractFactory("QNTCToken");
+  const token = await Token.deploy("10000000000000000000000000");
+  console.log("QNTC deployed to:", token.address);
+}
+main().catch((err) => {
+  console.error(err);
+  process.exitCode = 1;
+});
 
